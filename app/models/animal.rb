2ecb46f2df360902c0bd9f38995a10c191214ac2
Rian Carter <R.Carter86@hotmail.com>
2022-07-01 12:24:28 -0700

    add validations for species, breed, age, name, and gender

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,2 +1,7 @@
 class Animal < ApplicationRecord
+  validates :species, presence: :true
+  validates :breed, presence: :true
+  validates :age, presence: :true
+  validates :name, presence: :true
+  validates :gender, presence: :true
 end
