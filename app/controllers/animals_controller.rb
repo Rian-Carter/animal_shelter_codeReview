6c1c22f8a37d5e8ff90b81ebe2c0e50e98d12cf2
Rian Carter <R.Carter86@hotmail.com>
2022-07-01 09:46:43 -0700

    add update method including response message

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -13,4 +13,15 @@ class AnimalsController < ApplicationController
     @animal = Animal.create!(animal_params)
     json_response(@animal, :created)
   end
+
+  def update
+    @animal = Animal.find(params[:id])
+    if @animal.update!(animal_params)
+      render status: 200, json: {
+        message: "Created animal successfully."
+      }
+    end
+  end
+
+  
 end
