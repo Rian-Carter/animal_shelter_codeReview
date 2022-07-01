46d30a286290ca4b195ad756c9e11fd8eb4037c3
Rian Carter <R.Carter86@hotmail.com>
2022-07-01 15:04:37 -0700

    move all v1 testing to v1 directory

---
+++ spec/requests/v1/delete_animal_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe 'Delete animal route', :type => :request do
+  let!(:animals) { FactoryBot.create_list(:animal, 20)}
+
+  before do
+    @animal_id = Animal.first.id
+    delete "/api/v1/animals/#{@animal_id}"
+  end
+
+  it 'returns a deleted success status message' do
+    expect(JSON.parse(response.body)['message']).to eq("Deleted animal successfully.")
+  end
+
+  it 'returns status code 200' do
+    expect(response).to have_http_status(200)
+  end
+
+  it 'returns a 404 status not found' do
+    delete "/api/v1/animals/1000"
+    expect(response.status).to eq(404)
+  end
+end
