2e3aa607aacc2cdb8f51bd59e618e42a8a28f94d
Rian Carter <R.Carter86@hotmail.com>
2022-07-01 15:31:54 -0700

    passing test for limiting pagination to 5

---
+++ spec/requests/v2/animals_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "animals API", :type => :request do
+  let!(:animals) { FactoryBot.create_list(:animal, 20)}
+
+  describe 'Get /animals?page=3' do
+    before do
+      Animal.create(:species => 'Cat', :breed => "Tabby", :age => 14, :name => "Gary", :gender => "Female")
+    end
+
+    it 'returns a subset of animals based on pagination' do
+      get '/api/v2/animals?page=1', params: { limit: 5 }
+      expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body).length).to eq(5)
+    end
+  end
+end
