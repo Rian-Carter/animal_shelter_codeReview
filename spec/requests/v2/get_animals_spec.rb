8bf7c155d846d75a9987f8ca1bf62c58ee5f60d3
Rian Carter <R.Carter86@hotmail.com>
2022-07-01 15:14:21 -0700

    add 2 passing tests based on pagination in v2

---
+++ spec/requests/v2/get_animals_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "get all animals route", :type => :request do
+  let!(:animals) {FactoryBot.create_list(:animal, 20)}
+
+  before { get '/api/v2/animals'}
+
+  it 'returns all animals' do
+    expect(JSON.parse(response.body).size).to eq(5)
+  end
+
+  it 'returns status code 200' do
+    expect(response).to have_http_status(:success)
+  end
+end
