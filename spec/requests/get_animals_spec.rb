2090b0c8c659210a6cc0a3cb6f1b745f78f748df
Rian Carter <R.Carter86@hotmail.com>
2022-07-01 12:06:56 -0700

    second test passing. returns status code 200

--- spec/requests/get_animals_spec.rb
+++ spec/requests/get_animals_spec.rb
@@ -9,4 +9,7 @@ describe "get all animals route", :type => :request do
     expect(JSON.parse(response.body).size).to eq(20)
   end
 
+  it 'returns status code 200' do
+    expect(response).to have_http_status(:success)
+  end
 end
