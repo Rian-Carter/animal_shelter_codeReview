0e623635ad9e1ff87d00b60f1d6ca6be5ad3f65b
Rian Carter <R.Carter86@hotmail.com>
2022-07-01 09:41:44 -0700

    build out seeds using faker for dogs and cats

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+class Seed
+
+  def self.begin
+    seed = Seed.new
+    seed.create_animals
+  end
+
+  def create_animals
+    100.times do |i|
+      animal = Animal.create!(
+        species: "Dog",
+        breed: Faker::Creature::Dog.breed,
+        age: Faker::Number.within(range: 1..13),
+        name: Faker::Creature::Dog.name,
+        gender: Faker::Gender.binary_type
+      )
+      puts "Dog #{i} created"
+    end
+
+    100.times do |i|
+      animal = Animal.create!(
+        species: "Cat",
+        breed: Faker::Creature::Cat.breed,
+        age: Faker::Number.within(range: 1..13),
+        name: Faker::Creature::Cat.name,
+        gender: Faker::Gender.binary_type
+      )
+      puts "Cat #{i} created"
+    end
+  end
+end
+
+Seed.begin
